1fdf3400dcfc2ff7f5e5dea41c1b3e0ad48c837e
OumaimaTiguint <oumaimatiguint@gmail.com>
2024-02-27 16:10:44 +0100

    updated mongoose and fixed deprecation warnings

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri);
 
 const connection = mongoose.connection;
 connection.once('open', () => {
